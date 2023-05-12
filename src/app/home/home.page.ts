0a9d7a5463c52332344649062766bd3df233593b
Ulises Rayón <uliarp@ciencias.unam.mx>
2023-05-12 04:53:00 -0600

    added tabs for filtering and change buttons to checkboxes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,17 +2,21 @@ import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { Todo } from '../todo.model';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
   todos: Todo[] = [
     {id : '1', task: 'Buy Milk', completed: false},
     {id : '2', task: 'Pick up laundry',completed:true},
     {id : '3', task: 'Learn Ionic', completed:false},
   ];
+  
+  filter : 'all' | 'completed' | 'uncompleted' = 'uncompleted';
 
   constructor(private alertController: AlertController){}
 
@@ -27,6 +31,33 @@ export class HomePage {
 
   removeTask(id:string){
     this.todos = this.todos.filter(todo => todo.id !== id )
+  }
+
+  markAsDone(id:string){
+    this.todos = this.todos.map(
+      todo => {
+        if (todo.id === id){
+          return {...todo,
+          completed : true
+          };
+        } else {
+          return todo;
+        }
+      });
+  }
+
+  changeFilter(filter: 'all' | 'completed' | 'uncompleted'){
+    this.filter = filter;
+  }
+
+  get filteredTodos() {
+    if (this.filter === 'completed') {
+      return this.todos.filter(t => t.completed);
+    } else if (this.filter === 'uncompleted') {
+      return this.todos.filter(t => !t.completed);
+    } else {
+      return this.todos
+    }
   }
 
   async showAddTaskAlert(){
