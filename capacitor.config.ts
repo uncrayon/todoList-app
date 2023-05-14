fd34ccd8e9db0f5e8ffc1b7f0cd7ec83a3a5291a
Ulises Rayón <uliarp@ciencias.unam.mx>
2023-05-14 16:03:59 -0600

    new feature branch

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'todo',
+  appName: 'ur todo',
   webDir: 'www',
   server: {
     androidScheme: 'https'
