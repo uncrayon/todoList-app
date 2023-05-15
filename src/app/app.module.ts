12c29b380f7aba17f4a1f989378c405041ccc68b
Ulises Rayón <uliarp@ciencias.unam.mx>
2023-05-15 06:40:25 -0600

    connection work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import firebase from 'firebase/app';
-import 'firebase/database';
-import 'firebase/auth'
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8D53PBYj7WvyJ1bam72ejqAlxvwkU9Cw",
+  authDomain: "utodo-app.firebaseapp.com",
+  databaseURL: "https://utodo-app-default-rtdb.firebaseio.com",
+  projectId: "utodo-app",
+  storageBucket: "utodo-app.appspot.com",
+  messagingSenderId: "970271552177",
+  appId: "1:970271552177:android:feb433ddd2b9a4d9ba6890"
+};
+const app = initializeApp(firebaseConfig);
+
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from "firebase/auth";
+
+const db = getFirestore(app);
+const auth = getAuth(app);
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -19,14 +34,3 @@ import { AppRoutingModule } from './app-routing.module';
 })
 export class AppModule {}
 
-const firebaseConfig :any = {
-  apiKey: "AIzaSyD8D53PBYj7WvyJ1bam72ejqAlxvwkU9Cw",
-  authDomain: "utodo-app.firebaseapp.com",
-  databaseURL: "https://utodo-app-default-rtdb.firebaseio.com",
-  project-id: "utodo-app",
-  storageBucket: "utodo-app.appspot.com",
-  massagingSenderId: "970271552177",
-  appId: "1:970271552177:android:feb433ddd2b9a4d9ba6890"
-};
-
-firebase.initializeApp(firebaseConfig);
