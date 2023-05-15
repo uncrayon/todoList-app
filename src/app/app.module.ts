55c4db7cf9b76a7273a8be75904f0d2e73079609
Ulises Rayón <uliarp@ciencias.unam.mx>
2023-05-15 06:16:25 -0600

    added firebase credentials 4 debbug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth'
+
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
@@ -14,3 +18,15 @@ import { AppRoutingModule } from './app-routing.module';
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8D53PBYj7WvyJ1bam72ejqAlxvwkU9Cw",
+  authDomain: "utodo-app.firebaseapp.com",
+  databaseURL: "https://utodo-app-default-rtdb.firebaseio.com",
+  project-id: "utodo-app",
+  storageBucket: "utodo-app.appspot.com",
+  massagingSenderId: "970271552177",
+  appId: "1:970271552177:android:feb433ddd2b9a4d9ba6890"
+};
+
+firebase.initializeApp(firebaseConfig);
